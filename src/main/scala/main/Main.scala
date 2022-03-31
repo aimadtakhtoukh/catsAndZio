2888e4e265d9357c1e45e7e52646379f1ae236f9
Aimad Takhtoukh <aimad.takhtoukh@gmail.com>
2022-03-31 17:29:50 +0200

    Bad Future : we're not waiting

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,10 +1,11 @@
 package main
 
-import scala.util.Try
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends App {
-  def effect(int : String) : Try[Int] = {
-    Try { int.toInt }
+  def effect(int : String) : Future[Int] = {
+    Future { Thread.sleep(5000); int.toInt }
   }
 
   def sideEffect(int: Int): Unit = {
