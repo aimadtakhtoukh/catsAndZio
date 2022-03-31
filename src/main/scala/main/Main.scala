b53bb06a117daa82894f0fd66529ab2ea081419e
Aimad Takhtoukh <aimad.takhtoukh@gmail.com>
2022-03-31 17:33:07 +0200

    Better, but completely synchronous

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,22 +1,29 @@
 package main
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 
 object Main extends App {
   def effect(int : String) : Future[Int] = {
-    Future { Thread.sleep(5000); int.toInt }
+    Future { Thread.sleep(1500); int.toInt }
   }
 
   def sideEffect(int: Int): Unit = {
     println(int)
   }
 
-  effect("5")
-    .map(sideEffect)
-    .recover(_.printStackTrace())
+  Await.ready(
+    effect("5")
+      .map(sideEffect)
+      .recover(_.printStackTrace()),
+    Duration.Inf
+  )
 
-  effect("cinq")
-    .map(sideEffect)
-    .recover(_.printStackTrace())
+  Await.ready(
+    effect("cinq")
+      .map(sideEffect)
+      .recover(_.printStackTrace()),
+    Duration.Inf
+  )
 }
