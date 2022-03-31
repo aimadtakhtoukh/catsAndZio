5f0b16ce4e31cf57ccdd2d0a62ee8f97047dbf3a
Aimad Takhtoukh <aimad.takhtoukh@gmail.com>
2022-03-31 16:47:22 +0200

    Using Try to handle exceptions

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,18 +1,21 @@
 package main
 
+import scala.util.Try
+
 object Main extends App {
-  def effect(int : String) : Int = {
-    int.toInt
+  def effect(int : String) : Try[Int] = {
+    Try { int.toInt }
   }
 
   def sideEffect(int: Int): Unit = {
     println(int)
   }
 
-  try {
-    val eff = effect("cinq")
-    sideEffect(eff)
-  } catch {
-    case e: Throwable => e.printStackTrace()
-  }
+  effect("5")
+    .map(sideEffect)
+    .recover(_.printStackTrace())
+
+  effect("cinq")
+    .map(sideEffect)
+    .recover(_.printStackTrace())
 }
